16e6d86e0d24ee4d73736ae7c59676586c0acb19
Chetan Pawar <chetanpawar2002official@gmail.com>
2026-09-09 12:42:19 +0530

    app added

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 # res= tavily_search("best hotels in goa")
 # print(res)
 
-res= search_flights("Plan a 7 days Japan trip from india")
-print(res)
+# res= search_flights("Plan a 7 days Japan trip from india")
+# print(res)
+
+user_input= input("enter travel request: ")
+response= run_travel_agent(
+    user_input=user_input,
+    thread_id='test_user'
+)
+print("\nFinal Response: \n")
+print(response['answer'])
